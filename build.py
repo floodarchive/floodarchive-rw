20cbdca7a2ab4de1462736b7b1a03df001be87d4
Umut Erozan <umut_erozan@hotmail.com>
2022-09-09 13:03:59 +0300

    update build

--- build.py
+++ build.py
@@ -2,14 +2,15 @@ from json import loads
 from markdown import markdown
 from urllib3 import PoolManager
 from datetime import datetime as dt
+from db_connection import db, cursor
 
 LIMIT = 100
 SUBREDDIT = "kopyamakarna"
 IGNORE_FLAIRS = ["META", "DUYURU"]
 API_BASE_URL = "https://api.pushshift.io/reddit/search/submission"
 
-after = 1540846800  # Subreddit created_utc
-all_submissions = []
+cursor.execute("SELECT created_utc from floods ORDER BY id DESC LIMIT 1")
+after = cursor.fetchone()["created_utc"] or 1540846800  # Subreddit created_utc
 http = PoolManager()
 
 while True:
@@ -23,6 +24,7 @@ while True:
         break
     submissions = loads(response.data.decode("utf-8"))["data"]
     if not len(submissions):
+        print("Up to date, no new data in API...")
         break
     after = submissions[-1]["created_utc"]
 
@@ -36,13 +38,15 @@ while True:
         if selftext.strip() in "<p>[removed]</p>":
             continue
 
-        all_submissions.append(
-            {
-                "title": submission.get("title"),
-                "selftext": selftext,
-                "full_link": submission.get("full_link"),
-                "created": dt.fromtimestamp(
-                    submission.get("created_utc")
-                ).ctime(),
-            }
+        sql = "INSERT INTO floods (title, selftext, full_link, created, created_utc) VALUES (%s, %s, %s, %s, %s)"
+        val = (
+            submission.get("title"),
+            selftext,
+            submission.get("full_link"),
+            dt.fromtimestamp(submission.get("created_utc")).ctime(),
+            submission.get("created_utc")
         )
+
+        cursor.execute(sql, val)
+        db.commit()
+        print("Data successfully inserted into MySql database...")
