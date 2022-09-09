7b909f0a321d79e0aca3b40453ff5aaaa89685a8
Umut Erozan <umut_erozan@hotmail.com>
2022-09-09 13:04:08 +0300

    update main

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 from db_connection import cursor
+from datetime import datetime as dt
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():        
-    return render_template("index.html")
+def index():
+    cursor.execute("SELECT * from floods")
+    floods = cursor.fetchall()
+
+    cursor.execute("SELECT created_utc from floods ORDER BY id DESC LIMIT 1")
+    last_row_created = dt.fromtimestamp(cursor.fetchone()["created_utc"]).ctime()
+
+    return render_template("index.html", submissions=floods, last_flood_date=last_row_created)
 
 @app.route("/about")
 def about():
